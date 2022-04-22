765585737ee24a583d92f2a0e367a01071b67822
dayoungMM <dalbeetm@gmail.com>
2022-04-22 19:29:12 +0900

    파일 재배치

--- effective_java/src/item3/ElvisStaticFactory.java
+++ effective_java/src/item3/ElvisStaticFactory.java
@@ -1,10 +1,13 @@
 package item3;
 
 public class ElvisStaticFactory {
-    public static final ElvisStaticFactory INSTANCE = new ElvisStaticFactory();
+    //static 멤버 private으로 막고
+    private static final ElvisStaticFactory INSTANCE = new ElvisStaticFactory();
     private ElvisStaticFactory() {
         System.out.println("private Elvis");
     }
+
+    //메서드로 인스턴스를 가져옴
     public static ElvisStaticFactory getInstance() {return INSTANCE;}
     public void leaveTheBuilding() {
         System.out.println("leaveTheBuilding");
