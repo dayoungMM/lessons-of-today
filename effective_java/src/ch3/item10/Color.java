5202ff38c75c49a071c7d969c422bb6e3659931a
dayoungMM <dalbeetm@gmail.com>
2022-04-24 23:56:25 +0900

    equals는 일반 규약을 지켜서 재정의하라

---
+++ effective_java/src/ch3/item10/Color.java
@@ -0,0 +1,4 @@
+package ch3.item10;
+
+
+public enum Color { RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET }
---
+++ effective_java/src/ch3/item10/ColorPoint.java
@@ -0,0 +1,10 @@
+package ch3.item10;
+
+public class ColorPoint extends Point{
+    private final Color color;
+
+    public ColorPoint(int x, int y, Color color) {
+        super(x, y);
+        this.color = color;
+    }
+}
