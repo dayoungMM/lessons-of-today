691eb552dd48dacb2011876c649071f95a41972c
dayoungMM <dalbeetm@gmail.com>
2022-05-09 19:01:32 +0900

    방어적 복사본

---
+++ effective_java/src/ch8/item50/Period.java
@@ -0,0 +1,28 @@
+package ch8.item50;
+
+import java.util.Date;
+
+
+public final class Period {
+    private final Date start;
+    private final Date end;
+
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
+
+        if (this.start.compareTo(end) > 0)
+            throw new IllegalArgumentException(
+                start + "가" + end + " 보다 늦다."
+            );
+
+    }
+
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    public Date end() {
+        return new Date(end.getTime());
+    }
+}
